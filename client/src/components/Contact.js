4fed3595a220f5530b01cb190facd06286e35074
Darryl J. Zeigler <Dzeig.j@gmail.com>
2020-08-28 20:25:14 -0400

    update form autocomplete

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import './Contact.css';
 
 export default class Contact extends Component {
   state = {
@@ -50,18 +51,18 @@ export default class Contact extends Component {
     return(
       <form id="contact" className="contact-form" onSubmit={ (e) => this.formSubmit(e)}>
         <label className="message-name" htmlFor="message-name">Your Name</label>
-        <input onChange={ e => this.setState({ name: e.target.value })} name="name" className="message-name" type="text" placeholder="Your Name" required value={this.state.name}/>
+        <input autoComplete="none" onChange={ e => this.setState({ name: e.target.value })} name="name" className="message-name" type="text" placeholder="Your Name" required value={this.state.name}/>
 
         <label className="message-email" htmlFor="message-email">Your Email</label>
-        <input onChange={ (e) => this.setState({ email: e.target.value })} name="email" className="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
+        <input autoComplete="on" onChange={ (e) => this.setState({ email: e.target.value })} name="email" className="message-email" type="email" placeholder="your@email.com" required value={this.state.email} />
 
         <label className="message-subject" htmlFor="message-subject">Your Subject</label>
-        <input onChange={ e => this.setState({ subject: e.target.value })} name="subject" className="message-name" type="text" placeholder="Subject" required value={this.state.subject}/>
+        <input autoComplete="none" onChange={ e => this.setState({ subject: e.target.value })} name="subject" className="message-name" type="text" placeholder="Subject" required value={this.state.subject}/>
 
         <label className="message" htmlFor="message-input">Your Message</label>
         <textarea onChange={ e => this.setState({ message: e.target.value })} name="message" className="message-input" type="text" placeholder="Please write your message here" required value={this.state.message} />
 
-        <div className="button--container">
+        <div className="button-container">
           <button disabled={ this.state.disabled } type="submit" className="button button-primary">{ this.state.buttonText }</button>
         </div>
       </form>
